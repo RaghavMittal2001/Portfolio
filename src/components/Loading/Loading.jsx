0f506291a55630da6f24fdf1694a34f92142d60c
RaghavMittal2001 <raghavmittal1389@gmail.com>
2025-04-22 13:12:09 +0530

    fix

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -4,7 +4,7 @@ import "./Loading.scss";
 
 const Loading = ({ size = "medium", color = "primary" }) => {
   return (
-    <div className={`spinner-container ${size} ${color}`}>
+    <div className={`spinner-container ${size} ${color}`}> 
       <div className="spinner"></div>
     </div>
   );
